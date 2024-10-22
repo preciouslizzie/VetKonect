84ae408e8380dde0784052e1654b4f741cb76f5c
Precious <oryaelizabeth98@gmail.com>
2024-10-22 22:50:47 +0100

    v4

--- lib/Drawer/drawer Menu/Pets and livestock/tab_conroller.dart
+++ lib/Drawer/drawer Menu/Pets and livestock/tab_conroller.dart
@@ -1,5 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class TabControllerX extends GetxController {
+class TabControllerX extends GetxController   {
+  late TabController tabController;
   var isPetsTab = true.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    tabController.addListener(() {
+      if (tabController.index == 0) {
+        isPetsTab.value = true;
+      } else {
+        isPetsTab.value = false;
+      }
+    });
+  }
+
+  @override
+  void onClose() {
+    tabController.dispose();
+    super.onClose();
+  }
 }
